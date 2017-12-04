f42b033cf0a5331586da2f07e517f28243471c28
James Dalboth <James.Dalboth@gmail.com>
2017-12-04 18:56:21 +0000

    Extra

--- src/Player.java
+++ src/Player.java
@@ -3,12 +3,21 @@ public class Player {
   private Color color;
   private Board board;
   private boolean isComputerPlayer;
+  private Player opp;
 
   public Player(Game gm, Board brd, Color clr, boolean isCP){
     game = gm;
     color = clr;
     board = brd;
     isComputerPlayer = isCP;
+  }
+
+  public void setOpponent(Player opponent){
+    opp = opponent;
+  }
+
+  public Color getColor(){
+    return color;
   }
 
 
